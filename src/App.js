389e2a9d3de36797831fd75c1390f073acadc61d
bini0104 <goqlsdl0120@gmail.com>
2024-06-23 21:44:57 +0900

    1:1문의 답변

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Customer from './pages/Customer/Customer';
 import CustomerDetail from './pages/Customer/Customerdetail';
 import Honeypot from './pages/Honeypot/Honeypot';
 import HoneypotDetail from './pages/Honeypot/Honeypotdetail';
+import Inquiry from './pages/Customer/Inquiry';
 import '../src/pages/Home/Dashboard.css';
 import '../src/pages/Customer/Customer.css';
 import '../src/pages/Customer/Customerdetail.css';
@@ -14,24 +15,25 @@ import { GlobalStyles } from '@mui/material';
 import Sidebar from './components/commons/Sidebar';
 export default function App() {
 
-    return (
-   <>      
-    <GlobalStyles/>
+  return (
+    <>
+      <GlobalStyles />
       <BrowserRouter>
-         <div style={{ display: 'flex' }}>
-        <Sidebar />
-        <div style={{ flex: 1, padding: '16px', overflowY: 'auto',height:'100%' }}>
-          <Routes>
-            <Route index element={<Dashboard />} />
-            <Route path="/home" element={<Dashboard />} />
-            <Route path="/customer" element={<Customer />} />
-            <Route path="/customer/:id" element={<CustomerDetail />} />
-            <Route path='/honeypot' element={<Honeypot />} />
-            <Route path="/honeypot/:no" element={<HoneypotDetail />} />
-          </Routes>
+        <div style={{ display: 'flex' }}>
+          <Sidebar />
+          <div style={{ flex: 1, padding: '16px', overflowY: 'auto', height: '100%' }}>
+            <Routes>
+              <Route index element={<Dashboard />} />
+              <Route path="/home" element={<Dashboard />} />
+              <Route path="/customer" element={<Customer />} />
+              <Route path="/customer/:id" element={<CustomerDetail />} />
+              <Route path='/honeypot' element={<Honeypot />} />
+              <Route path="/honeypot/:no" element={<HoneypotDetail />} />
+              <Route path='/inquiryanswer' element={<Inquiry />} />
+            </Routes>
+          </div>
         </div>
-      </div>
       </BrowserRouter>
-     </>
-    );
+    </>
+  );
 }
---
+++ src/pages/Customer/Inquiry.js
@@ -0,0 +1,103 @@
+import { useState } from "react";
+import style from "../Customer/Inquiry.module.css";
+import { useNavigate } from 'react-router-dom';
+
+function InquiryAnswer() {
+
+    const navigater = useNavigate();
+
+    const [answer, setAnswer] = useState("");
+    const handleAnswer = (e) => {
+        setAnswer(e.target.value);
+    }
+
+    const [modalOpen, setModalOpen] = useState(false);
+    const [writerModal, setWriterModal] = useState(false);
+
+    const handleSubmit = () => {
+
+        if (answer !== "") {
+            console.log("답변:", answer);
+            setModalOpen(true);
+        } else {
+            setWriterModal(true);
+        }
+    }
+
+    const closeBtn = () => {
+        setModalOpen(false);
+        setWriterModal(false);
+    }
+
+    const [checkModal, setCheckModal] = useState(false);
+
+    const handleCancel = () => {
+        if (answer !== "") {
+            setCheckModal(true);
+        } else {
+            navigater(-1);
+        }
+    }
+
+    return (
+        <>
+            <div className={style.wrapper}>
+                <div className={style.contentBox}>
+                    <p className={style.pageTitle}>1:1문의 답변</p>
+
+                    <p className={style.categoryBox}>유형</p>
+                    <p className={style.titleBox}>제목</p>
+                    <p className={style.contextBox}>내용</p>
+
+                    <textarea className={style.answerBox} type="text" name="answer" placeholder="답변을 입력해주세요." value={answer} onChange={handleAnswer}></textarea>
+                    <div className={style.buttons}>
+                        <a>
+                            <button type='button' className={style.cancelButton} onClick={() => { handleCancel() }}>취소</button>
+                        </a>
+                        <button type='submit' className={style.submitButton} onClick={() => { handleSubmit() }}>등록</button>
+                    </div>
+
+                    {modalOpen && (
+                        <div className={style.back}>
+                            <div className={style.modal}>
+                                <img src='/images/commons/icon_confirm.png' alt='확인' width={45} />
+                                <p className={style.modalTitle}>1:1문의가 접수 되었습니다.</p>
+                                <p className={style.modalContext}>문의 내용에 따라 답변이 늦어질 수 있습니다.</p>
+                                <a href="/help">
+                                    <button className={style.modalButton} onClick={closeBtn}>확인</button>
+                                </a>
+                            </div>
+                        </div>
+                    )}
+                    {writerModal && (
+                        <div className={style.back}>
+                            <div className={style.modal}>
+                                <img src='/images/commons/icon_alert.png' alt='경고' width={45} />
+                                <p className={style.modalTitle}>제목과 내용을 모두 작성해주세요.</p>
+                                <button className={style.modalButton} onClick={closeBtn}>확인</button>
+                            </div>
+                        </div>
+                    )}
+                    {checkModal && (
+                        <div className={style.back}>
+                            <div className={style.modal}>
+                                <img src='/images/commons/icon_alert.png' alt='경고' width={45} />
+                                <p className={style.modalTitle}>1:1문의 작성을 취소하시겠습니까?</p>
+                                <p className={style.modalContext}>작성 취소된 내용은 되돌릴 수 없습니다.</p>
+                                <div className={style.modalButtonBox}>
+                                    <button className={style.modalButton} onClick={() => setCheckModal(false)}>취소</button>
+                                    <button className={style.modalButton} onClick={() => navigater(-1)}>확인</button>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </>
+
+    )
+
+
+}
+
+export default InquiryAnswer;
