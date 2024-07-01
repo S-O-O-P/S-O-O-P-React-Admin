68725c12eee3feee9fe188c0d65febbbb44e468c
Bbini0104 <goqlsdl0120@gmail.com>
2024-07-01 12:04:31 +0900

    되돌리기..

--- src/main/java/com/soop/jwtsecurity/config/SecurityConfig.java
+++ src/main/java/com/soop/jwtsecurity/config/SecurityConfig.java
@@ -1,88 +1,88 @@
-//package com.soop.jwtsecurity.config;
-//
-//import com.soop.jwtsecurity.handler.CustomSuccessHandler;
-//import com.soop.jwtsecurity.jwt.JWTFilter;
-//import com.soop.jwtsecurity.jwt.JWTUtil;
-//import com.soop.jwtsecurity.service.CustomOAuth2UserService;
-//import jakarta.servlet.http.HttpServletRequest;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.CorsConfigurationSource;
-//
-//import java.util.Arrays;
-//import java.util.Collections;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    private final CustomOAuth2UserService customOAuth2UserService;
-//    private final CustomSuccessHandler customSuccessHandler;
-//    private final JWTUtil jwtUtil;
-//
-//    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService, CustomSuccessHandler customSuccessHandler, JWTUtil jwtUtil) {
-//        this.customOAuth2UserService = customOAuth2UserService;
-//        this.customSuccessHandler = customSuccessHandler;
-//        this.jwtUtil = jwtUtil;
-//    }
-//
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//
-//        // CORS 설정
-//        http.cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
-//            @Override
-//            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-//                CorsConfiguration configuration = new CorsConfiguration();
-//
-//
-//                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000")); // 허용할 출처를 설정
-//                configuration.setAllowedMethods(Collections.singletonList("*")); // 모든 HTTP 메서드(GET, POST, PUT, DELETE 등)를 허용
-//                configuration.setAllowCredentials(true); // 자격 증명(쿠키, Authorization 헤더 등)을 포함한 요청을 허용
-//                configuration.setAllowedHeaders(Collections.singletonList("*")); // 모든 헤더를 허용
-//                configuration.setMaxAge(600L); // CORS 설정을 브라우저에 캐시할 시간
-//                configuration.setExposedHeaders(Arrays.asList("Set-Cookie", "Authorization")); // 프론트로 반환할 헤더들  // 익스포즈드 headers 설정
-//
-//                return configuration;
-//            }
-//        }));
-//
-//        // CSRF 비활성화
-//        http.csrf((csrf) -> csrf.disable());
-//
-//        // Form 로그인 비활성화
-//        http.formLogin((form) -> form.disable());
-//
-//        // HTTP Basic 인증 비활성화
-//        http.httpBasic((basic) -> basic.disable());
-//
-//        // JWT 필터 추가
-//        http.addFilterAfter(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
-//
-//        // OAuth2 로그인 설정
-//        http.oauth2Login((oauth2) -> oauth2
-//                .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
-//                        .userService(customOAuth2UserService))
-//                .successHandler(customSuccessHandler)
-//        );
-//
-//        // 경로별 인가 설정
-//        http.authorizeHttpRequests((auth) -> auth
-//                .requestMatchers("/main", "/").permitAll() // 메인 페이지 접근 허용
-//                .requestMatchers("/reissue").permitAll()   // 토큰 재발급 경로 접근 허용
-//                .anyRequest().authenticated()              // 기타 모든 요청은 인증 필요
-//        );
-//
-//        // 세션 관리 설정: STATELESS
-//        http.sessionManagement((session) -> session
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // 세션을 사용하지 않음
-//
-//        return http.build();
-//    }
-//}
+package com.soop.jwtsecurity.config;
+
+import com.soop.jwtsecurity.handler.CustomSuccessHandler;
+import com.soop.jwtsecurity.jwt.JWTFilter;
+import com.soop.jwtsecurity.jwt.JWTUtil;
+import com.soop.jwtsecurity.service.CustomOAuth2UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomSuccessHandler customSuccessHandler;
+    private final JWTUtil jwtUtil;
+
+    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService, CustomSuccessHandler customSuccessHandler, JWTUtil jwtUtil) {
+        this.customOAuth2UserService = customOAuth2UserService;
+        this.customSuccessHandler = customSuccessHandler;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // CORS 설정
+        http.cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration configuration = new CorsConfiguration();
+
+
+                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000")); // 허용할 출처를 설정
+                configuration.setAllowedMethods(Collections.singletonList("*")); // 모든 HTTP 메서드(GET, POST, PUT, DELETE 등)를 허용
+                configuration.setAllowCredentials(true); // 자격 증명(쿠키, Authorization 헤더 등)을 포함한 요청을 허용
+                configuration.setAllowedHeaders(Collections.singletonList("*")); // 모든 헤더를 허용
+                configuration.setMaxAge(600L); // CORS 설정을 브라우저에 캐시할 시간
+                configuration.setExposedHeaders(Arrays.asList("Set-Cookie", "Authorization")); // 프론트로 반환할 헤더들  // 익스포즈드 headers 설정
+
+                return configuration;
+            }
+        }));
+
+        // CSRF 비활성화
+        http.csrf((csrf) -> csrf.disable());
+
+        // Form 로그인 비활성화
+        http.formLogin((form) -> form.disable());
+
+        // HTTP Basic 인증 비활성화
+        http.httpBasic((basic) -> basic.disable());
+
+        // JWT 필터 추가
+        http.addFilterAfter(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+        // OAuth2 로그인 설정
+        http.oauth2Login((oauth2) -> oauth2
+                .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
+                        .userService(customOAuth2UserService))
+                .successHandler(customSuccessHandler)
+        );
+
+        // 경로별 인가 설정
+        http.authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/main", "/").permitAll() // 메인 페이지 접근 허용
+                .requestMatchers("/reissue").permitAll()   // 토큰 재발급 경로 접근 허용
+                .anyRequest().authenticated()              // 기타 모든 요청은 인증 필요
+        );
+
+        // 세션 관리 설정: STATELESS
+        http.sessionManagement((session) -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // 세션을 사용하지 않음
+
+        return http.build();
+    }
+}
--- src/main/java/com/soop/jwtsecurity/service/CustomOAuth2UserService.java
+++ src/main/java/com/soop/jwtsecurity/service/CustomOAuth2UserService.java
@@ -1,87 +1,87 @@
-//package com.soop.jwtsecurity.service;
-//
-//import com.soop.jwtsecurity.dto.*;
-//import com.soop.jwtsecurity.entityDTO.UserEntity;
-//import com.soop.jwtsecurity.mapper.UserMapper;
-//import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-//import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-//import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-//import org.springframework.security.oauth2.core.user.OAuth2User;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@Service
-//public class CustomOAuth2UserService extends DefaultOAuth2UserService {
-//
-//    private UserMapper userMapper;
-//
-//    public CustomOAuth2UserService(UserMapper userMapper) {
-//        this.userMapper=userMapper;
-//    }
-//
-//    @Override
-//    @Transactional
-//    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-//
-//        OAuth2User oAuth2User = super.loadUser(userRequest);
-//
-//
-//        // 어디서 오는 정보인지 확인
-//        String registrationId = userRequest.getClientRegistration().getRegistrationId();
-//        OAuth2Response oAuth2Response = null;
-//
-//        if (registrationId.equals("naver")) {
-//
-//            oAuth2Response = new NaverResponse(oAuth2User.getAttributes());
-//        } else if (registrationId.equals("google")) {
-//
-//            oAuth2Response = new GoogleResponse(oAuth2User.getAttributes());
-//
-//        } else if (registrationId.equals("kakao")) {
-//
-//            oAuth2Response = new KakaoResponse(oAuth2User.getAttributes());
-//        } else {
-//
-//            return null;
-//        }
-//
-//        //리소스 서버에서 발급 받은 정보로 사용자를 특정할 아이디값을 만듬
-//        String username = oAuth2Response.getProvider() + " " + oAuth2Response.getProviderId();
-//        System.out.println("username = " + username);
-//        UserEntity existData = userMapper.findByUsername(username);
-//
-//        if (existData == null) {
-//
-//            UserEntity userEntity = new UserEntity();
-//
-//            userEntity.setUsername(username);
-//            userEntity.setEmail(oAuth2Response.getEmail());
-//            userEntity.setName(oAuth2Response.getName());
-//            userEntity.setRole("ROLE_USER");
-//
-//            userMapper.saveUserEntity(userEntity);
-//
-//            UserDTO userDTO = new UserDTO();
-//            userDTO.setUsername(username);
-//            userDTO.setName(oAuth2Response.getName());
-//            userDTO.setRole("ROLE_USER");
-//
-//            return new CustomOAuth2User(userDTO);
-//        } else {
-//
-//            existData.setEmail(oAuth2Response.getEmail());
-//            existData.setName(oAuth2Response.getName());
-//            UserDTO userDTO = new UserDTO();
-//
-//            userDTO.setEmail(existData.getEmail());
-//            userDTO.setName(existData.getName());
-//
-//            userDTO.setUsername(existData.getUsername());
-//            userDTO.setName(oAuth2Response.getName());
-//            userDTO.setRole(existData.getRole());
-//
-//
-//            return new CustomOAuth2User(userDTO);
-//        }
-//    }
-//}
+package com.soop.jwtsecurity.service;
+
+import com.soop.jwtsecurity.dto.*;
+import com.soop.jwtsecurity.entityDTO.UserEntity;
+import com.soop.jwtsecurity.mapper.UserMapper;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private UserMapper userMapper;
+
+    public CustomOAuth2UserService(UserMapper userMapper) {
+        this.userMapper=userMapper;
+    }
+
+    @Override
+    @Transactional
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+
+        // 어디서 오는 정보인지 확인
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        OAuth2Response oAuth2Response = null;
+
+        if (registrationId.equals("naver")) {
+
+            oAuth2Response = new NaverResponse(oAuth2User.getAttributes());
+        } else if (registrationId.equals("google")) {
+
+            oAuth2Response = new GoogleResponse(oAuth2User.getAttributes());
+
+        } else if (registrationId.equals("kakao")) {
+
+            oAuth2Response = new KakaoResponse(oAuth2User.getAttributes());
+        } else {
+
+            return null;
+        }
+
+        //리소스 서버에서 발급 받은 정보로 사용자를 특정할 아이디값을 만듬
+        String username = oAuth2Response.getProvider() + " " + oAuth2Response.getProviderId();
+        System.out.println("username = " + username);
+        UserEntity existData = userMapper.findByUsername(username);
+
+        if (existData == null) {
+
+            UserEntity userEntity = new UserEntity();
+
+            userEntity.setUsername(username);
+            userEntity.setEmail(oAuth2Response.getEmail());
+            userEntity.setName(oAuth2Response.getName());
+            userEntity.setRole("ROLE_USER");
+
+            userMapper.saveUserEntity(userEntity);
+
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUsername(username);
+            userDTO.setName(oAuth2Response.getName());
+            userDTO.setRole("ROLE_USER");
+
+            return new CustomOAuth2User(userDTO);
+        } else {
+
+            existData.setEmail(oAuth2Response.getEmail());
+            existData.setName(oAuth2Response.getName());
+            UserDTO userDTO = new UserDTO();
+
+            userDTO.setEmail(existData.getEmail());
+            userDTO.setName(existData.getName());
+
+            userDTO.setUsername(existData.getUsername());
+            userDTO.setName(oAuth2Response.getName());
+            userDTO.setRole(existData.getRole());
+
+
+            return new CustomOAuth2User(userDTO);
+        }
+    }
+}
