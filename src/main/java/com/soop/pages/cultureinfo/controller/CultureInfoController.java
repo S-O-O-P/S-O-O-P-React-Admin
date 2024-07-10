c10f0334661ea9fd13189e87b5a350ab5a0fc3fa
shubiduba1212 <dldks1212@gmail.com>
2024-07-10 10:58:58 +0900

    modify CultureInfoController.java for requestMapping

--- src/main/java/com/soop/pages/cultureinfo/controller/CultureInfoController.java
+++ src/main/java/com/soop/pages/cultureinfo/controller/CultureInfoController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/cultureInfo")
+@RequestMapping("/cultureinfo")
 public class CultureInfoController {
   private final CultureInfoService cultureInfoService;
 
